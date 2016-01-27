afffdc1a4cab6a38b550f60632f1212cd35dd770
Reshama <reshama@thisismetis.com>
2016-01-27 15:55:57 -0500

    Update datetime_month.py

--- python/datetime_month.py
+++ python/datetime_month.py
@@ -4,3 +4,9 @@
 #bin Release Dates by month
 movies['RD_month']=pd.DatetimeIndex(movies['ReleaseDate']).month
 ```
+
+```
+# extract month from datetime64
+mv_df['month'] =  pd.DatetimeIndex(mv_df['reldate']).month
+print mv_df[:2] 
+```
